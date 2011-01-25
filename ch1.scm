a4e1ad681b7c793e6ac0780f8e2a77446761cea5
Mike Prentice <mjp44@buffalo.edu>
2011-01-25 09:36:22 -0500

    working on exercise 1.19

--- ch1.scm
+++ ch1.scm
@@ -190,3 +190,21 @@
           (else (fast-*-iter (+ acc a) a (- b 1)))))
   (fast-*-iter 0 a b))
 
+;;; Exercise 1.19
+;;; =============
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   <??>
+                   <??>
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
