ee81f72b3ad046a8f3247754c00b120c29744b39
Mike Prentice <mjp44@buffalo.edu>
2011-02-20 23:38:22 -0500

    up to exercise 1.28

--- ch1.scm
+++ ch1.scm
@@ -208,3 +208,96 @@
                         p
                         q
                         (- count 1)))))
+
+;;; Exercise 1.22
+;;; =============
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define runtime current-milliseconds) ;;; for running in Racket
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))
+      null))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start end)
+  (cond ((even? start) (search-for-primes (+ start 1) end))
+        ((even? end) (search-for-primes start (- end 1)))
+        ((= start end) (timed-prime-test end))
+        (else (timed-prime-test start)
+              (search-for-primes (+ start 2) end))))
+  
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else (remainder (* base (expmod base (- exp 1) m))
+                         m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+;;; Exercise 1.27
+;;; =============
+
+(define (carmichael? n)
+  (define (carmichael-iter a n)
+    (cond ((= a n) #t)
+          ((= (expmod a n n) a) (carmichael-iter (+ 1 a) n))
+          (else #f)))
+  (and (carmichael-iter 2 n) (not (prime? n))))
+
+;;; Exercise 1.28
+;;; =============
+
+(define (expmod-1.28 base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (if (and (not (or (= base 1)
+                           (= base (- m 1))))
+                  (= (remainder (square base) m) 1))
+             0
+             (remainder (square (expmod-1.28 base (/ exp 2) m))
+                        m)))
+        (else (remainder (* base (expmod-1.28 base (- exp 1) m))
+                         m))))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (not (= (expmod-1.28 a (- n 1) n) 0)))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime-1.28? n times)
+  (cond ((= times 0) true)
+        ((miller-rabin-test n) (fast-prime-1.28? n (- times 1)))
+        (else false)))
