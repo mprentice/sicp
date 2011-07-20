c5283316d2418bb37e54e04b0fe79d0437fae2e6
Mike Prentice <mjp44@buffalo.edu>
2011-07-20 17:38:18 -0400

    some chapter 3.3.4 exercises

--- ch3.scm
+++ ch3.scm
@@ -112,4 +112,91 @@
 
 ;;; Section 3.1.2
 
+;;; Section 3.3.4
+
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+(define d (make-wire))
+(define e (make-wire))
+(define s (make-wire))
+
+(or-gate a b d)
+(and-gate a b c)
+(inverter c e)
+(and-gate d e s)
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+;;; Exercise 3.28
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+;;; Exercise 3.29
+
+;;; 2 inverter-delay + 1 and-gate-delay
+;;; a1 --|>o--+
+;;;           |
+;;;           +--|--\
+;;;              |   |--|>o-- output
+;;;           +--|--/
+;;;           |
+;;; a2 --|>o--+
+
+(define (or-gate-2 a1 a2 output)
+  (let ((w1 (make-wire))
+        (w2 (make-wire))
+        (w3 (make-wire)))
+    (inverter a1 w1)
+    (inverter a2 w2)
+    (and-gate w1 w2 w3)
+    (inverter w3 output)
+    'ok))
 
