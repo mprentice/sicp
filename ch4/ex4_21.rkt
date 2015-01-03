af3504ac17912ebd8a703b94d3899c689a787ead
Mike Prentice <mprentice@gmail.com>
2015-01-02 21:40:42 -0500

    Ex 4.21

---
+++ ch4/ex4_21.rkt
@@ -0,0 +1,35 @@
+;;; Ex 4.21
+
+;;; a.
+
+((lambda (n)
+   ((lambda (fact)
+      (fact fact n))
+    (lambda (ft k)
+      (if (= k 1)
+          1
+          (* k (ft ft (- k 1)))))))
+ 10)
+
+;;; ==> 3628800
+
+;;; b.
+
+(define (f x)
+  (define (even? n)
+    (if (= n 0)
+        true
+        (odd? (- n 1))))
+  (define (odd? n)
+    (if (= n 0)
+        false
+        (even? (- n 1))))
+  (even? x))
+
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false (ev? ev? od? (- n 1))))))
